161e417e496ed2b2f2be5869efea643d1455bb85
liubomyr.gavryliv <liubomyr.gavryliv@gmail.com>
2021-11-17 12:31:14 +0100

    create linear models depending on discovery rate

--- main/regression_model.py
+++ main/regression_model.py
@@ -1,20 +1,20 @@
 import pandas as pd
 import numpy as np
 
-from sklearn.linear_model import LinearRegression, Ridge
-from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-from functions.helpers import parse_mindat, parse_rruff, get_discovery_rate_all, get_discovery_rate_endemic, get_endemic_proportion
+from functions.helpers import parse_mindat, parse_rruff, get_discovery_rate_all, get_discovery_rate_endemic, \
+    get_endemic_proportion
 
 # -*- coding: utf-8 -*-
 """
 @author: Liubomyr Gavryliv
-Code for training a Regression model on discovery rates of all and endemic minerals
+Code for training two Regression models on discovery rates of endemic minerals between 1900-2012 and 1900-2021 
 """
 
 locs_md = pd.read_csv('data/mindat_locs.csv', sep=',')
@@ -32,6 +32,7 @@ mindat_rruff = mindat_rruff[['discovery_year', 'locality_counts']]
 discovery_rate_all = get_discovery_rate_all(mindat_rruff)
 discovery_rate_endemic = get_discovery_rate_endemic(mindat_rruff)
 endemic_proportion = get_endemic_proportion(discovery_rate_endemic, discovery_rate_all)
+endemic_proportion.reset_index(inplace=True)
 
 # Plot endemic proportion (endemic count / all count * 100%)
 
@@ -47,37 +48,50 @@ plt.savefig(f"figures/endemic_minerals/proportion.jpeg", dpi=300, format='jpeg')
 plt.close()
 
 
-# Create a model of before 2012, use without outliers and calculate True endemicity
+
+# Create different linear models for a subsets of years
 # Here X (attribute) is discovery year and Y (label) is number of endemic minerals discovered during that year
 
-transformer = PolynomialFeatures(degree=2, include_bias=False)
+transformer = PolynomialFeatures(degree=3, include_bias=False)
+post_1900 = endemic_proportion.loc[(endemic_proportion['discovery_year'] > 1900)]
 
-X_all = endemic_proportion[['count_all']]
-X_all.reset_index(inplace=True)
-X_all = np.array(X_all[['discovery_year']], dtype=int)
-X = X_all[(X_all > 1900) & (X_all < 2012)].reshape(-1,1)
-X_after_1900 = X_all[(X_all > 1900)].reshape(-1,1)
+X = {
+    '1900_2021': post_1900[['discovery_year']].to_numpy(dtype=int).reshape(-1, 1),
+}
 
-X_ = transformer.fit_transform(X)
-X_after_1900_ = transformer.fit_transform(X_after_1900)
+# polynomial transformed
+X_ = {
+    '1900_2021': transformer.fit_transform(X['1900_2021']),
+}
 
-y_all = endemic_proportion.reset_index().to_numpy(dtype=int)
-y = y_all[(y_all[:, 0] > 1900) & (y_all[:, 0] < 2012)][:, 1]
+y = {
+    '1900_2021': post_1900[['count_endemic']].to_numpy(dtype=int)
+}
 
-X_train, X_test, y_train, y_test = train_test_split(X_, y, test_size=0.2, random_state=0, shuffle=False)
-
-model = LinearRegression(fit_intercept=True)
-
-model.fit(X_, y)
-
-model.score(X_, y)
-
-predicted_after_1900 = model.predict(X_after_1900_)
-
-# Plot the model
-plt.scatter(y_all[(y_all[:, 0] > 1900)][:, 0], y_all[(y_all[:, 0] > 1900)][:, 1], color='green', marker='o', s=20,
+# Plot initial discovery rate of endemic minerals
+plt.scatter(post_1900[['discovery_year']], post_1900[['count_endemic']], color='green', marker='o', s=20,
             edgecolors='black', linewidths=0.1)
-plt.plot(X_after_1900, predicted_after_1900, color="blue", linewidth=1)
+
+training_years = [2000, 2012, 2021]
+
+for training_year in training_years:
+
+    pre_year = post_1900.loc[post_1900['discovery_year'] <= training_year]
+
+    X['pre_year'] = pre_year[['discovery_year']].to_numpy(dtype=int).reshape(-1, 1)
+
+    # polynomial transformed
+    X_['pre_year'] = transformer.fit_transform(X['pre_year'])
+
+    y['pre_year'] = pre_year[['count_endemic']].to_numpy(dtype=int)
+
+    model = LinearRegression(fit_intercept=True)
+    model.fit(X_['pre_year'], y['pre_year'])
+    print(f'R2 = {model.score(X_["1900_2021"], y["1900_2021"])}')
+    predicted = model.predict(X_['1900_2021'])
+
+
+    plt.plot(post_1900[['discovery_year']], predicted, color="blue", linewidth=1)
 
 plt.savefig(f"figures/endemic_minerals/linear_regression.jpeg", dpi=300, format='jpeg')
 
