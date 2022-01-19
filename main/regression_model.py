38ae83383b7c46ff9d78671e49d451cd18419d6c
liubomyr.gavryliv <liubomyr.gavryliv@gmail.com>
2022-01-19 21:05:46 +0100

    merge 2- and 3-rd polynomial transforms

--- main/regression_model.py
+++ main/regression_model.py
@@ -40,81 +40,6 @@ endemic_proportion = get_endemic_proportion(discovery_rate_endemic, discovery_ra
 endemic_proportion.reset_index(inplace=True)
 
 
-# Create different linear models for a subsets of years
-# Here X (attribute) is discovery year and Y (label) is number of endemic minerals discovered during that year
-
-transformer = PolynomialFeatures(degree=2, include_bias=False)
-post_1900 = endemic_proportion.loc[(endemic_proportion['discovery_year'] > 1900)]
-
-X = {
-    '1900_2021': post_1900[['discovery_year']].to_numpy(dtype=int).reshape(-1, 1),
-}
-
-# polynomial transformed
-X_ = {
-    '1900_2021': transformer.fit_transform(X['1900_2021']),
-}
-
-y = {
-    '1900_2021': post_1900[['count_endemic']].to_numpy(dtype=int)
-}
-
-sns.set_theme(palette=None, style={ 'figure.facecolor': 'white', 'xtick.bottom': True, 'ytick.left': True })
-# Plot initial discovery rate of endemic minerals
-plt.scatter(post_1900[['discovery_year']], post_1900[['count_endemic']], color='green', marker='o', s=20,
-            edgecolors='black', linewidths=0.1)
-
-training_sets = [
-    { 'year': 1990, 'color': 'green', 'linestyle': 'dotted' },
-    { 'year': 2000, 'color': 'magenta', 'linestyle': 'dotted' },
-    { 'year': 2011, 'color': 'blue', 'linestyle': 'dashed' },
-    { 'year': 2021, 'color': 'olive', 'linestyle': 'dashed' }
-]
-
-for training_set in training_sets:
-
-    pre_year = post_1900.loc[post_1900['discovery_year'] <= training_set['year']]
-
-    X['pre_year'] = pre_year[['discovery_year']].to_numpy(dtype=int).reshape(-1, 1)
-
-    # polynomial transformed
-    X_['pre_year'] = transformer.fit_transform(X['pre_year'])
-    y['pre_year'] = pre_year[['count_endemic']].to_numpy(dtype=int)
-
-    X_train, X_test, y_train, y_test = train_test_split(X_['pre_year'], y['pre_year'], test_size = 0.1, random_state = 53, # 51 and 52
-                                                        shuffle=True)
-
-    model = LinearRegression(fit_intercept=True)
-    model.fit(X_train, y_train)
-    # model.fit(X_['pre_year'], y['pre_year'])
-
-    y_pred_all = model.predict(X_['1900_2021'])
-    y_pred = model.predict(X_test)
-
-    # Calculate RMSE and r2
-    RMSE = np.sqrt(mean_squared_error(y_test, y_pred))
-    R2 = r2_score(y_test, y_pred)
-    exp_variance = explained_variance_score(y_test, y_pred)
-
-    print(f'1900-{training_set["year"]} range \n'
-          f'R2 = {R2};\n'
-          f'RMSE = {RMSE}; \n'
-          f'Intercept: {model.intercept_} \n'
-          f'Coefficients: {model.coef_}; \n'
-          f'Explained variance: {exp_variance} \n'
-          f'----------------------------------')
-
-    plt.plot(post_1900[['discovery_year']], y_pred_all, color=training_set['color'], linestyle=training_set['linestyle'], linewidth=1)
-
-plt.xlabel('Discovery year')
-plt.ylabel('Number of observed endemic minerals')
-
-plt.savefig(f"figures/endemic_minerals/linear_regression_2_polynomial.jpeg", dpi=300, format='jpeg')
-
-plt.close()
-
-
-
 # Use seaborn to compile regression plots
 
 
@@ -185,23 +110,26 @@ for index, training_set in enumerate(training_sets):
 
 
 sns.set_theme(palette=None, style={ 'figure.facecolor': 'white', 'xtick.bottom': True, 'ytick.left': True })
+plt.rcParams['axes.titlepad'] = -14
 
-g = sns.FacetGrid(all, col="type", col_wrap=2, height=4, aspect=1, margin_titles=True, despine=False)
+g = sns.FacetGrid(all, col="type", col_wrap=2, height=4, aspect=1, margin_titles=True, despine=False, legend_out=True)
 g.map(sns.scatterplot, "x", "features_all", color='green', marker='o', s=80, edgecolor='black', linewidth=1,
       alpha=0.5)
 g.map(sns.scatterplot, "x", "features_range", color='teal', marker='o', s=80, alpha=0.8, edgecolor='black', linewidth=1)
-g.map(sns.regplot, "x", "features_range", order=3, truncate=False, scatter=False, ci=95, color='dodgerblue')
+g.map(sns.regplot, "x", "features_range", order=2, truncate=False, scatter=False, ci=95, color='dodgerblue', line_kws= {'alpha':0.6, 'linewidth': 1})
+g.map(sns.regplot, "x", "features_range", order=3, truncate=False, scatter=False, ci=95, color='orange', line_kws= {'alpha':0.6, 'linewidth': 1})
 
 g.set_axis_labels("Discovery year", "Minerals count")
 g.set_titles(col_template="{col_name}")
 g.tight_layout()
+g.add_legend(labels=['2nd degree polynomial regression', '3rd degree polynomial regression',  'features', 'training set'])
 g.figure.subplots_adjust(wspace=0, hspace=0)
 g.set(ylim=(-10, 100))
 
 plt.xlabel('Discovery year')
 plt.ylabel('Minerals count')
 
-plt.savefig(f"figures/endemic_minerals/linear_regression_2_polynomial_sns.jpeg", dpi=300, format='jpeg')
+plt.savefig(f"figures/endemic_minerals/linear_regression.jpeg", dpi=300, format='jpeg')
 
 plt.close()
 
